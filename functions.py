a02ce1b1d55dd79ed4bf60698ce4518ee630e5c9
Ihor <Korniienkoihor@gmail.com>
2023-09-29 16:22:51 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,27 @@
+# custom function:
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    # Doc string:
+    """ Read a text file and return
+    the list of to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write to-do item list in the file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("The text is:")
+    print(get_todos())
+
+# to read the documentation :
+#print(get_todos.__doc__)
+#print(write_todos.__doc__)
