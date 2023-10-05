64cb304d3c951229516ce15fd342d52a4a29c5b6
Ihor <Korniienkoihor@gmail.com>
2023-10-06 00:29:06 +0200

    Implement the Complete and Exit buttons to App

--- gui.py
+++ gui.py
@@ -2,14 +2,21 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type a To-Do list")
-input_box = sg.InputText(tooltip="Enter todo", key="todo")
+input_box = sg.InputText(tooltip="Enter todo", key='todo')
 add_button = sg.Button("Add")
-list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
+
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
 # layout expects a list of object instances (e.g. buttons, text boxes)
 window = sg.Window('To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -24,6 +31,7 @@ while True:
             todos.append(new_todos)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update('')
         case "Edit":
             todo_to_edit = value['todos'][0]
             new_todo = value['todo']
@@ -32,6 +40,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = value['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update('')
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=value['todos'][0])
         case sg.WINDOW_CLOSED:
