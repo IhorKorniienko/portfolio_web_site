8f78d326a8aab4521ac68e858502450ff5873831
Ihor <Korniienkoihor@gmail.com>
2023-10-13 11:33:17 +0200

    add button_background and position, change theme of the app and font of the input text

--- gui.py
+++ gui.py
@@ -3,30 +3,33 @@ import PySimpleGUI as sg
 import time
 
 clock = sg.Text("", key='clock')
-sg.theme("DarkGreen")
+sg.theme("Black")
 
-label = sg.Text("Type a To-Do list")
-input_box = sg.InputText(tooltip="Enter todo", key='todo')
-add_button = sg.Button("Add")
+# Adding input box & list box, labels
+label = sg.Text("Type a To-Do list:", font=("Times New Roman", 18))
+input_box = sg.InputText(tooltip="Enter todo", key='todo', size=[55], border_width=6)
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True, size=[35, 10])
+                      enable_events=True, size=[60, 15], horizontal_scroll=True)
 
-edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
-exit_button = sg.Button("Exit")
+# Adding buttons
+add_button = sg.Button("Add", font=("Times New Roman", 15), button_color="Yellow", border_width=0)
+added_buttons = [[sg.Button("Edit", font=("Times New Roman", 15), button_color="Yellow", border_width=0)],
+                 [sg.Button("Complete", font=("Times New Roman", 15), button_color="Yellow", border_width=0)],
+                 [sg.Button("Exit", font=("Times New Roman", 15), button_color="Yellow", border_width=0)]]
+
+layout_edit_complete_exit = sg.Column(added_buttons, vertical_alignment='Top')
 
 # layout expects a list of object instances (e.g. buttons, text boxes)
 window = sg.Window('To-Do App',
                    layout=[[clock],
                            [label],
                            [input_box, add_button],
-                           [list_box, edit_button, complete_button],
-                           [exit_button]],
-                   font=('Helvetica', 20))
+                           [list_box, layout_edit_complete_exit]],
+                   font=('Times New Roman', 14))
 
 while True:
     event, value = window.read(timeout=200)
-    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"), background_color="Black")
 
     match event:
         case "Add":
@@ -47,7 +50,8 @@ while True:
                 functions.write_todos(todos)
                 window['todos'].update(values=todos)
             except IndexError:
-                sg.popup("Please select an item first and update it and press Edit to confirm your changes!", font=("Helvetica", 20))
+                sg.popup("Please select an item first and update it and press Edit to confirm your changes!",
+                         font=("Times New Roman", 14))
 
         case "Complete":
             try:
@@ -58,16 +62,17 @@ while True:
                 window['todos'].update(values=todos)
                 window['todo'].update('')
             except IndexError:
-                sg.popup("You have to select an item and press Complete!", font=("Helvetica", 20))
+                sg.popup("You have to select an item and press Complete!", font=("Times New Roman", 14))
 
         case "Exit":
             break
 
         case 'todos':
-            window['todo'].update(value=value['todos'][0])
+            try:
+                window['todo'].update(value=value['todos'][0])
+            except IndexError:
+                sg.popup("You cannot select the empty field!", font=("Times New Roman", 14))
 
         case sg.WINDOW_CLOSED:
             break
 window.close()
-
-
