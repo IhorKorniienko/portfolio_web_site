de8f9af544169fc8b58be02dbae543f52469b28d
Ihor <Korniienkoihor@gmail.com>
2024-08-25 00:05:52 +0200

    Create columns, image and description

---
+++ webpage_portfolio/main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.PNG")
+
+with col2:
+    st.title("Ihor Korniienko")
+
+    content = """Results-driven Data Analyst with over 6 years of experience in leveraging advanced analytical 
+    capabilities and providing actionable insights in fast-paced environments. Proficient in Python, with a strong 
+    foundation in object-oriented programming principles to develop scalable data solutions. Expertise in 
+    Databricks SQL for data modeling and querying large datasets efficiently. Adept at visualizing data trends and 
+    patterns to support strategic decision-making. Excellent communication skills with a proven ability to collaborate 
+    with cross-functional teams to drive business improvements
+    """
+    st.info(content)
